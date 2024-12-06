6e8d52e85de4d929e313c86a78ac6b15a9ce64c7
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-12-06 11:45:08 -0800

    add password

---
+++ app/login/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Main, Section, Container } from "@/components/craft";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { loginAccess } from "@/lib/actions";
+import { toast } from "sonner";
+
+export default function Page() {
+  async function handleLogin(formData: FormData) {
+    const result = await loginAccess(formData);
+    toast.error(result.error);
+  }
+
+  return (
+    <Main>
+      <Section>
+        <Container className="border border-gray-200 rounded-lg p-4 shadow-sm max-w-xl">
+          {/* @ts-expect-error - what? */}
+          <form action={handleLogin} className="flex flex-col gap-2">
+            <Input
+              type="password"
+              name="password"
+              required
+              placeholder="Password"
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Container>
+      </Section>
+    </Main>
+  );
+}
---
+++ lib/actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function loginAccess(formData: FormData) {
+  "use server";
+
+  const password = formData.get("password");
+  if (password !== process.env.PASSWORD) {
+    return { error: "Invalid password" };
+  }
+
+  cookies().set(
+    "call_sim_login",
+    Buffer.from(password as string).toString("base64")
+  );
+  redirect("/");
+}
